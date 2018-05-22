e76e134eaff505a2ef45891b04b3866d1090e2cb
Aubrey Neimeier <neimeier.1@osu.edu>
2018-05-22 17:31:34 +0000

    automatically backed up by learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
 module ApplicationHelper
-  def display_artist(song)
-    if song.artist.present?
-      #display link to artist
-      link_to artist_path(self), artist.name
-    else
-      #display link to edit the song page
-      link_to edit_song_path(self), "Edit Song"
-    end
-  end
+  
 end
